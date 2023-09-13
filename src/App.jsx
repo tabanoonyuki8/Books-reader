233bbdfc10ef56e72d7cdee2c75d7f886fa9cad0
Tabanoon mim <you@example.com>
2023-09-13 15:31:06 +0600

    updated ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Blogs from './Componenets/Blogs/Blogs'
+import Bookmarks from './Componenets/Booksmarks/Bookmarks'
 import Header from './Componenets/Header/Header'
 
 function App() {
@@ -9,7 +10,10 @@ function App() {
   return (
     <>
      <Header></Header>
-     <Blogs></Blogs>
+<div className='md:flex'>
+<Blogs></Blogs>
+<Bookmarks></Bookmarks>
+</div>
  
       
       
--- src/Componenets/Header/Header.jsx
+++ src/Componenets/Header/Header.jsx
@@ -1,7 +1,9 @@
+import profile from "../../assets/images/profile2.jpg.jpg"
 const Header = () => {
     return (
-        <div>
-            <h2 className="text-5xl font-bold">Book lovers</h2>
+        <div className='flex justify-between items-center p-4 mx-4 border-b-2'>
+            <h2 className="text-5xl font-bold ">Book lovers</h2>
+            <img className="w-16 h-16 rounded" src={profile} alt="" />
         </div>
     );
 };
