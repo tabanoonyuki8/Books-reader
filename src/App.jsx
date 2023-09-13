8b76312fddf17bf66b115683dfdf02935b0ecb16
Tabanoon mim <you@example.com>
2023-09-13 14:44:38 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Blogs from './Componenets/Blogs/Blogs'
+import Header from './Componenets/Header/Header'
+
+function App() {
+  
+
+  return (
+    <>
+     <Header></Header>
+     <Blogs></Blogs>
+ 
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Componenets/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h2 className="text-5xl font-bold">Book lovers</h2>
+        </div>
+    );
+};
+
+export default Header;
